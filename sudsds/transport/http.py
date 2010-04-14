cc31d72b48b42d9b0f98107dfc6da60fc55ad00b
Beda Kosata <bedrich.kosata@nic.cz>
2010-04-14 11:37:16 +0200

    add README to sudsds, update README in dslib

--- sudsds/transport/http.py
+++ sudsds/transport/http.py
@@ -14,6 +14,8 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 # written by: Jeff Ortel ( jortel@redhat.com )
 
+# modifications by: Beda Kosata (bedrich.kosata@nic.cz)
+
 """
 Contains classes for basic HTTP transport implementations.
 """
